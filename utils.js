c599fd80da9c953d911e659f3d528f7cc78b0aea
Ryan Shaw <ryanshaw@unc.edu>
2013-04-18 06:40:16 -0400

    Revert "Convert to SPA."
    This reverts commit 42e84685472817d1babe4ccfa372a2c7ebf7eadb.

--- utils.js
+++ utils.js
@@ -13,6 +13,7 @@ function initializeWebApp() {
     app.set('view engine', 'ejs');
     app.set('view options', { layout: false });
     app.use(express.bodyParser());
+    app.use(express.methodOverride());
     app.use(app.router);
     app.use(express.static(__dirname + '/public'));
     app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
@@ -30,10 +31,8 @@ function connectToDatabase(name) {
       db.exists(function (err, exists) {
         if (err) { console.log('error', err); } 
         else if (! exists) {
-          db.create(function(err) {
-            if (err) { console.error(err); }
-            else { console.log('Created new database.'); }
-          });
+          db.create();
+          console.log('Created new database.');
         }      
       });
       this.save = function() { 
